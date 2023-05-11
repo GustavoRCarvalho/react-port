3cbb13df9761a091ab4363dfdfe1cf11540a5d61
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-05-11 04:38:38 -0300

    add: menu tabs

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,9 +1,25 @@
-import { Button } from "@mui/material"
+import Box from "@mui/material/Box"
+import Tab from "@mui/material/Tab"
+import TabContext from "@mui/lab/TabContext"
+import TabList from "@mui/lab/TabList"
+import TabPanel from "@mui/lab/TabPanel"
+import { useState } from "react"
 
 export function Menu() {
+  const [value, setValue] = useState("1")
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
   return (
-    <div>
-      <Button variant="contained">BOTAO</Button>
-    </div>
+    <TabContext value={value}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+          <Tab label="Gráfico" value="Gráfico" />
+        </TabList>
+      </Box>
+      <TabPanel value="1">Item One</TabPanel>
+      <TabPanel value="Gráfico">Gráfico</TabPanel>
+    </TabContext>
   )
 }
